7974a9ea2e4878f8d5983495a7d61eb181cbf736
netakussia <nicolaiplesca62@gmail.com>
2025-03-30 16:41:48 +0300

    Adaug fișierele proiectului

---
+++ script.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.rubrica .eseu').forEach(function(titlu) {
+    titlu.addEventListener('click', function() {
+        this.classList.toggle('active'); // Togglează clasa 'active' pe fiecare titlu
+    });
+});
+
+        window.onscroll = function() {
+            var menu = document.getElementById("menu");
+            if (window.pageYOffset > 100) {  // Când scroll-ul ajunge la 100px
+                menu.classList.add("sticky");
+            } else {
+                menu.classList.remove("sticky");
+            }
+        };
+        window.addEventListener("scroll", function() {
+        var sections = document.querySelectorAll("section");
+        var scrollPosition = window.scrollY + window.innerHeight;
+        
+        sections.forEach(function(section) {
+            if (section.offsetTop <= scrollPosition && section.offsetTop + section.offsetHeight > scrollPosition) {
+                document.body.style.backgroundColor = getComputedStyle(section).backgroundColor;
+            }
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+    const scrollButton = document.getElementById('scrollTop');
+    if (document.documentElement.scrollTop > 300) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+});
+
+document.getElementById('scrollTop').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+// --------------------------------------------------------------------------------
+
